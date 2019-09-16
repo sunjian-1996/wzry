80baf0969380ea4b39855417ef6e89b5a965cd0f
luhan <luhan@itcast.cn>
2019-09-16 15:36:35 +0800

    孙健：测试

--- royal_service/src/main/java/com/bbs/service/ArticleService.java
+++
@@ -1,9 +0,0 @@
-package com.bbs.service;
-
-import com.bbs.domain.Article;
-
-import java.util.List;
-
-public interface ArticleService {
-    List<Article> findAll(int zoneId) throws Exception;
-}
--- royal_service/src/main/java/com/bbs/service/impl/ArticleServiceImpl.java
+++
@@ -1,8 +0,0 @@
-package com.bbs.service.impl;
-
-import com.bbs.service.ArticleService;
-import org.springframework.stereotype.Service;
-
-@Service("articleService")
-public class ArticleServiceImpl implements ArticleService {
-}
